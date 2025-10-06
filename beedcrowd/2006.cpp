a3bb878d976f165b894dcf22a5f874a07323599c
guilherme.andrd <nickyandrade331@gmail.com>
2025-10-06 00:00:38 -0300

    mais um

---
+++ beedcrowd/2006.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int n=0; cin >> n;
+
+    int i, s=0;
+
+    while(cin >> i){
+        if(i = n) s++;
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
